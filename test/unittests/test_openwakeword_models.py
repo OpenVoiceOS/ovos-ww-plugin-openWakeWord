6cb226520768309b0bbbf2232d47b417f221beae
Mike Gray <mike@graywind.org>
2024-04-17 22:42:09 -0500

    fix: support openwakeword 0.6.0

--- test/unittests/test_openwakeword_models.py
+++ test/unittests/test_openwakeword_models.py
@@ -3,6 +3,8 @@ import unittest
 import numpy as np
 import openwakeword
 
+from ovos_ww_plugin_openwakeword import OwwHotwordPlugin
+
 class TestOpenWakeWord_plugin(unittest.TestCase):
     @classmethod
     def setUpClass(self):
@@ -12,3 +14,10 @@ class TestOpenWakeWord_plugin(unittest.TestCase):
         for i in range(10):
             prediction = self.model.predict(np.zeros(1280).astype(np.int16))
         self.assertNotEqual(0.0, prediction[list(self.model.models.keys())[0]])
+
+    def test_default_plugin_loads(self):
+        # Make sure the plugin loads properly
+        OwwHotwordPlugin()
+
+if __name__ == '__main__':
+    unittest.main()
