60a73e4a317369d6af95a6fa14d9f9ac34e8b911
Mike Gray <mike@graywind.org>
2024-04-17 23:06:16 -0500

    test the plugin, not the upstream package

--- test/unittests/test_openwakeword_models.py
+++ test/unittests/test_openwakeword_models.py
@@ -8,16 +8,13 @@ from ovos_ww_plugin_openwakeword import OwwHotwordPlugin
 class TestOpenWakeWord_plugin(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.model = openwakeword.Model()  # load default models
+        self.model = OwwHotwordPlugin().model  # load default models
 
     def test_model_prediction(self):
         for i in range(10):
             prediction = self.model.predict(np.zeros(1280).astype(np.int16))
         self.assertNotEqual(0.0, prediction[list(self.model.models.keys())[0]])
 
-    def test_default_plugin_loads(self):
-        # Make sure the plugin loads properly
-        OwwHotwordPlugin()
 
 if __name__ == '__main__':
     unittest.main()
